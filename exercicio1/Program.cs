2afadb930ae1191186848be568d243b8b572dac6
Jgmor4es <161323279+Allanzxz@users.noreply.github.com>
2025-02-17 10:34:21 -0400

    add exercicio condicional

---
+++ exercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.Write("Possui autorização do responsável? (s/n): ");
+        string autorizacao = Console.ReadLine().Trim().ToLower();
+
+        if (idade >= 18 || autorizacao == "s")
+        {
+            Console.WriteLine("Entrada Liberada");
+        }
+        else
+        {
+            Console.WriteLine("Entrada não permitida");
+        }
--- exercicio1/obj/Debug/net9.0/exercicio1.AssemblyInfo.cs
+++ exercicio1/obj/Debug/net9.0/exercicio1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("condicionais")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e747f3f1f8077c115b4047b304b54d20e1e074e5")]
-[assembly: System.Reflection.AssemblyProductAttribute("condicionais")]
-[assembly: System.Reflection.AssemblyTitleAttribute("condicionais")]
+[assembly: System.Reflection.AssemblyProductAttribute("exercicio1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
