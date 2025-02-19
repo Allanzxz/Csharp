e2a0878a15219f2099cfba0bf6ce9fbf3951b21f
Allanzxz <161323279+Allanzxz@users.noreply.github.com>
2025-02-19 07:47:12 -0400

    add estrutura de repeticao while

---
+++ estrutura-repeticao/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Random numeroAleatorio = new Random();
+int numero = numeroAleatorio.Next(1, 101);
+int contador = 0;
+Console.WriteLine($"Número Alvo:{numero}");
+while (contador <= numero)
+{
+    Console.WriteLine($"Contador:{contador}.");
+    contador++;
+}
+Console.WriteLine($"Contador atingiu o número alvo:{contador}");
