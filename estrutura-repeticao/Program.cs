481a7f3de11cda8431016c876452a828b2941c62
Allanzxz <161323279+Allanzxz@users.noreply.github.com>
2025-02-19 11:05:25 -0400

    add exercicios estrutura de repetição

--- estrutura-repeticao/Program.cs
+++ estrutura-repeticao/Program.cs
@@ -1,33 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Random numeroAleatorio = new Random();
+﻿Random numeroAleatorio = new Random();
 int numero = numeroAleatorio.Next(1, 101);
 int contador = 0;
-Console.WriteLine($"Número Alvo:{numero}");
+Console.WriteLine($"Número Alvo: {numero}");
 while (contador <= numero)
 {
-    Console.WriteLine($"Contador:{contador}.");
+    Console.WriteLine($"Contador:{contador}");
     contador++;
 }
-Console.WriteLine($"Contador atingiu o número alvo:{contador}");
+Console.WriteLine($"Contador atingiu o número alvo: {contador}");
 
+// Estrutura de repetição do...while
 
 int numero1 = 0;
 
 do{
-    Console.WriteLine($"Número:{numero1}");
-    numero++;
-} while (numero < 10);
+    Console.WriteLine($"Número: {numero1}");
+    numero1++;
+} while (numero1 <10);
 
-
+// Estrutura de repetição for
 
 for (int i = 0; i <= 10; i++)
 {
-    Console.WriteLine($"Valor de i:{i}");
+    Console.WriteLine($"Valor de i: {i}");
 }
 
-string[] nomes = {"Wanderson", "Jeane", "Ryan"};
+// Estrutura de repetição foreach
+
+string[] nomes = { "Wanderson", "Jeane", "Ryan"};
 foreach (string nome in nomes)
 {
     Console.WriteLine(nome);
